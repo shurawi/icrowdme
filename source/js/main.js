1911346854a05c5771f6367dda3ac7504575bfd7
shurawi <denis.b.toropov@gmail.com>
2021-04-22 21:51:02 +0700

    adds adaptive to advatages.scss

--- source/js/main.js
+++ source/js/main.js
@@ -1,26 +1,26 @@
 //toggles state in advantages block's description
-var btnSmp = document.querySelector('.advantages__btn--smp');
-var btnTrs = document.querySelector('.advantages__btn--trs');
-var btnClb = document.querySelector('.advantages__btn--clb');
+var btnSmp = document.querySelector('.advantages__link--smp');
+var btnTrs = document.querySelector('.advantages__link--trs');
+var btnClb = document.querySelector('.advantages__link--clb');
 var advHead = document.querySelector('.advantages__head');
 
 btnSmp.addEventListener ("click", function () {
-  btnSmp.classList.add('advantages__btn--active');
-  btnTrs.classList.remove('advantages__btn--active');
-  btnClb.classList.remove('advantages__btn--active');
+  btnSmp.classList.add('advantages__link--active');
+  btnTrs.classList.remove('advantages__link--active');
+  btnClb.classList.remove('advantages__link--active');
   advHead.style.color = '#26a6d1';
 })
 
 btnTrs.addEventListener ("click", function () {
-  btnTrs.classList.add('advantages__btn--active');
-  btnClb.classList.remove('advantages__btn--active');
-  btnSmp.classList.remove('advantages__btn--active');
+  btnTrs.classList.add('advantages__link--active');
+  btnClb.classList.remove('advantages__link--active');
+  btnSmp.classList.remove('advantages__link--active');
   advHead.style.color = '#add136';
 })
 
 btnClb.addEventListener ("click", function () {
-  btnClb.classList.add('advantages__btn--active');
-  btnSmp.classList.remove('advantages__btn--active');
-  btnTrs.classList.remove('advantages__btn--active');
+  btnClb.classList.add('advantages__link--active');
+  btnSmp.classList.remove('advantages__link--active');
+  btnTrs.classList.remove('advantages__link--active');
   advHead.style.color = '#959595';
 })
